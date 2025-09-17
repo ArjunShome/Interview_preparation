ffa5c0739723a1630777f625593a8272c7e676ac
Arjun Shome <arjunshome111@gmail.com>
2025-09-18 01:39:45 +0530

    Update Uvicorn server port to 8004 for application launch

--- Projects/Middleware/AuthenticationAuthorization/main.py
+++ Projects/Middleware/AuthenticationAuthorization/main.py
@@ -251,4 +251,4 @@ async def check_products_access(user: User=Security(get_current_user, scopes=["u
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8005, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8004, reload=True)
